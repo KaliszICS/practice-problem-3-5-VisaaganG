de15a8d1b9af90741e8f2967b6856f538df7a64c
VisaaganG <343505582@tdsb.ca>
2026-04-08 16:03:36 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,37 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static String getFirstName(String name) {
+		String firstName;
+		name = name.trim();
+		int index = name.indexOf(" ");
+		firstName = name.substring(0, index);
+		return firstName;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+	public static String getLastName(String name) {
+		String lastName;
+		name = name.trim();
+		int index = name.lastIndexOf(" ");
+		lastName = name.substring(index + 1);
+		return lastName;
 }
+
+	public static boolean isValidName(String name) {
+		name = name.trim();
+		String firstName;
+
+		String lastName;
+		int firstindex = name.indexOf(" ");
+		if (firstindex == -1) return false;
+
+		firstName = name.substring(0, firstindex);
+		int lastindex = name.lastIndexOf(" ");
+
+		if (firstindex == -1 || firstindex != lastindex) return false;
+		
+		lastName = name.substring(lastindex + 1);
+		return firstName.length() >= 2 && lastName.length() >= 2;
+	}
+}
+
